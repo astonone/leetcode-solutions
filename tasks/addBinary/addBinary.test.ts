5385f28bb2fbfd0716176e44b581cc2d5346b8d4
viktor.kulygin <viktor.kulygin@coolbet.com>
2024-09-24 13:31:38 +0300

    implemented addBinary task

--- tasks/addBinary/addBinary.test.ts
+++ tasks/addBinary/addBinary.test.ts
@@ -9,4 +9,8 @@ describe('Add Binary', () => {
     it('should pass test case 2', () => {
         expect(addBinary("1010", "1011")).toEqual("10101");
     });
+
+    it('should pass test case 3', () => {
+        expect(addBinary("1111", "1111")).toEqual("11110");
+    });
 });
--- tasks/addBinary/addBinary.ts
+++ tasks/addBinary/addBinary.ts
@@ -1,3 +1,45 @@
 export function addBinary(a: string, b: string): string {
-    return '';
+    let sum = '';
+    let store = ['0'];
+
+    const maxLength = a.length > b.length ? a.length : b.length;
+    for (let i = 0; i < maxLength; i++) {
+
+        let aNum = a[a.length - 1 - i];
+        let bNum = b[b.length - 1 - i];
+        let currentDigit;
+
+        if (!aNum && !bNum) {
+            break;
+        }
+        if (!aNum) {
+            aNum = '0';
+        }
+        if (!bNum) {
+            bNum = '0';
+        }
+
+        if (Number(aNum) + Number(bNum) > 1) {
+            currentDigit = '0';
+            store[i + 1] = '1';
+        } else {
+            currentDigit = (Number(aNum) + Number(bNum)).toString();
+            store[i + 1] = '0';
+        }
+
+        if (Number(currentDigit) + Number(store[i]) > 1) {
+            sum = '0' + sum;
+            store[i + 1] = '1';
+        } else {
+            sum = (Number(currentDigit) + Number(store[i])).toString() + sum;
+        }
+    }
+
+    if (store[maxLength] !== '0') {
+        sum = store[maxLength] + sum;
+    }
+
+    return sum;
 }
+
+
