b433330623b39aff29d59f39b83073660c3ad813
viktor.kulygin <viktor.kulygin@coolbet.com>
2025-02-18 13:33:28 +0200

    implemented pascalsTriangle2 task

--- tasks/pascalsTriangle2/pascalsTriangle2.ts
+++ tasks/pascalsTriangle2/pascalsTriangle2.ts
@@ -1,3 +1,24 @@
 export function getRow(rowIndex: number): number[] {
-    return []
+    const result: number[][] = [];
+    for (let i = 0; i < rowIndex + 1; i++) {
+        const newRow: number[] = [];
+        if (i === 0) {
+            result.push([i + 1]);
+        } else if (i === 1) {
+            result.push([i, i]);
+        } else {
+            const prevRow = result[i - 1];
+
+            newRow.push(1);
+            for (let j = 0; j < prevRow.length; j++) {
+                if (j + 1 < prevRow.length) {
+                    newRow.push(prevRow[j] + prevRow[(j + 1)]);
+                }
+            }
+            newRow.push(1);
+
+            result.push(newRow);
+        }
+    }
+    return result[rowIndex];
 }
